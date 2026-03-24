cecdb12d441d03b8dd6bfdbb037f621af9d9b6c6
ihorkrasnozhon <worldofiigor@gmail.com>
2026-03-24 22:43:58 +0100

    counter final

--- counter.js
+++ counter.js
@@ -1,28 +1,52 @@
-function CounterPrototype(element) {
+function Counter(element, options={}) {
     this.root = element;
     this.resultElement = this.root.querySelector('[data-counter="result"]');
     this.score = parseFloat(this.resultElement.innerText);
 
-    document.addEventListener('click', this.init.bind(this));
+    const {
+        step = 1,
+        min = -Infinity,
+        max = Infinity
+    } = options;
+
+    this.step = step;
+    this.min = min;
+    this.max = max;
+
+    this.root.addEventListener('click', this.handleClick.bind(this));
+
 }
 
-CounterPrototype.prototype.init = function (element) {
+Counter.init = function (target, options) {
+    let allCounters;
+
+    if(typeof target === 'string'){
+        allCounters = document.querySelectorAll(target);
+    } else if (target instanceof HTMLElement) {
+        allCounters = [target];
+    } else if (target instanceof NodeList) {
+        allCounters = target;
+}
+    allCounters.forEach(element => {
+        new Counter(element, options);
+    });
+}
+
+Counter.prototype.handleClick = function (element) {
     const button = element.target.closest('.button');
     if (!button || !this.root.contains(button)) return 0;
     this.update(button.dataset.counter);
 }
 
-CounterPrototype.prototype.update = function(action) {
-    if (action==='increment' || action==='decrement') {
-        action==='increment' ? this.score+=this.step : this.score-=this.step;
+Counter.prototype.update = function(action) {
+        if(action==='increment' && this.score+this.step<this.max) {
+            this.score+=this.step;
+        } else if (action==='decrement' && this.score-this.step>this.min) {
+            this.score-=this.step
+        }
         this.resultElement.innerText = this.score;
-    }
 }
 
-const allCounters = document.querySelectorAll('[data-counter="container"]');
-allCounters.forEach(element => {
-    new CounterPrototype(element);
-});
 
-
+Counter.init('[data-counter="container"]');
 
